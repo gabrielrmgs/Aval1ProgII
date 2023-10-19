104d07f3a1a11f68455036eb577d09a84ec37896
gabriel <gabrielrdes@outlook.com>
2023-10-18 23:17:04 -0300

    avaliação

--- genotype.dart
+++ genotype.dart
@@ -1,5 +1,4 @@
 
-
 class Genotype{
   final _genotypes = {'AA':'A', 'Ai':'A', 'BB':'B', 'Bi':'B', 'AB':'AB', 'ii':'O'};
   final _genotype;
@@ -18,11 +17,25 @@ class Genotype{
   }
 
   List <String> get alleles{
-    return genotype.split('').toList();
+    return genotype.split('').toSet().toList();
   }
 
   List <String> get agglutinogens{
-    return genotype.split('').toList();
+    List <String> lista1 = [];
+    if (_genotypes[_genotype] == 'A') {
+      lista1.add('A');
+    }
+    if (_genotypes[_genotype] == 'B') {
+      lista1.add('B');
+    }
+    if (_genotypes[_genotype] == 'AB') {
+      lista1.add('A');
+      lista1.add('B');
+    }
+    if (_genotypes[_genotype] == 'O') {
+      lista1.add('');
+    }
+    return lista1;
   }
 
   List <String> get agglutinins{
@@ -31,50 +44,46 @@ class Genotype{
     if (_genotypes[_genotype] == 'A') {
       lista1.add('B');
     }
-
     if (_genotypes[_genotype] == 'B') {
       lista1.add('A');
     }
-
     if (_genotypes[_genotype] == 'AB') {
       lista1.add('');
     }
-
     if (_genotypes[_genotype] == 'O') {
       lista1.add('A');
       lista1.add('B');
     }
-
     return lista1;
   }
-
+  
   List <String> offsprings(Genotype g){
-    List <String> listaFinal = [];
-    var genotipo1 = genotype.split('');
-    var genotipo2 = g.genotype.split('');
-    var result1 = genotipo1[0] + genotipo2[0];
-    var result2 = genotipo1[1] + genotipo2[1];
-
-    if (result1 == result2){
-      listaFinal.add(result1);
-    } else {
-      listaFinal.add(result1);
-      listaFinal.add(result2);
+    List <String> possibilities = [];
+    List <String> genotipo1 = genotype.split('');
+    List <String> genotipo2 = g.genotype.split('');
+    possibilities.add (genotipo1[0] + genotipo2[0]);
+    possibilities.add (genotipo1[1] + genotipo2[1]);
+    possibilities.add (genotipo1[1] + genotipo2[0]);
+    possibilities.add (genotipo1[0] + genotipo2[1]);
+    for (var i = 0;  i < possibilities.length; i++){
+      if (possibilities[i] == "iA"){possibilities[i] = "Ai";}
+      else if (possibilities[i] == "iB"){possibilities[i] = "Bi";}
+      else if (possibilities[i] == "BA"){possibilities[i] = "AB";}
     }
-
-    return listaFinal;
+    possibilities.sort();
+    return possibilities.toSet().toList();
   }
 
-  bool compatible( g) {
-    if (this == 'ii') {
+  bool compatible(Genotype g) {
+    if (genotype == 'ii') {
       return true;
-    } else if ((this == "AA" || this == "Ai") &&
-      (g == "AA" || g == "AB" || g == "Ai")) {
+    } else if ((genotype == "AA" || genotype == "Ai") &&
+      (g.genotype == "AA" || g.genotype == "AB" || g.genotype == "Ai")) {
     return true;
-  } else if ((this == "BB" || this == "Bi") &&
-      (g == "BB" || g == "AB" || g == "Bi")) {
+  } else if ((genotype == "BB" || genotype == "Bi") &&
+      (g.genotype == "BB" || g.genotype == "AB" || g.genotype == "Bi")) {
     return true;
-  } else if (this == "AB" && (g == "AB")) {
+  } else if (genotype == "AB" && (g.genotype == "AB")) {
     return true;
   } else {
     return false;
@@ -85,6 +94,4 @@ class Genotype{
   String toString(){
     return '$genotype';
   }
-
-  
 }
--- individual.dart
+++ individual.dart
@@ -3,11 +3,12 @@ import 'genotype.dart';
 class Individual{
   late final Genotype _genotype;
   final genotipo;
-  late final _name;
-  int indivNum = 0;
+  var _name;
+  static int indivNum = 0;
 
-  Individual(this.genotipo, [this._name = 'Indiv']){
+  Individual(this.genotipo, [this._name = "Indiv"]){
     _genotype = Genotype(genotipo);
+    _name = namegenerator();
   }
 
   Genotype get genotype{
@@ -17,14 +18,15 @@ class Individual{
   String get name {
     return _name;
   }
-
+  
   String namegenerator(){
-    indivNum++;
-    return '${name}($indivNum)';
+    if(name == "Indiv"){
+    return '${name}${++indivNum}';
+    }else {return name;}
   }
 
   @override
   String toString() {
-    return '$name'+'${genotype.bloodType}';
+    return '$name'+'(${genotype.bloodType})';
   }
 }
